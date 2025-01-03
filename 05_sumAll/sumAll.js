6ead1d38982b4b2256d6e52635a84d63fcd2e21e
zmurphy <zmurph90@gmail.com>
2025-01-03 15:03:28 -0800

    first five completed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,12 @@
-const sumAll = function() {
+const sumAll = function(start, fin) {
+    if (start < 0 || fin < 0 || !Number.isInteger(start) || !Number.isInteger(fin)) return 'ERROR';
+    if (fin < start) [fin, start] = [start, fin];
 
+    total = 0
+    for(let i = start; i <= fin; i ++) {
+        total += i;
+    }
+    return total;
 };
 
 // Do not edit below this line
