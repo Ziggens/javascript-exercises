3ffd921ba1ddd05707d3a35c790ed46684958495
zmurphy <zmurph90@gmail.com>
2025-01-03 17:35:29 -0800

    finished all

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,10 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    return people.reduce((oldest, cur) => {
+        this_year = new Date().getFullYear()
+        cur_age = (cur.yearOfDeath ?? this_year )- cur.yearOfBirth;
+        oldest_age = (oldest.yearOfDeath ?? this_year) - oldest.yearOfBirth;
+        return oldest_age > cur_age ? oldest : cur
+    });
 };
 
 // Do not edit below this line
